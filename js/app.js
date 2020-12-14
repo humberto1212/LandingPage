b9775414d8113553603c1e5ac816f04114999d7d
humberto1212 <71103179+humberto1212@users.noreply.github.com>
2020-12-14 18:57:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@
 const sections = document.querySelectorAll('section')
 const nav = document.getElementById('nav_list');
 let manyList = ''
-             
+           
+
 // 1
 //list creation with class, href and dataset
 for (var i = 0; i < sections.length; i++) {
-    const list = `<li class="links_menu"><a href="#${sections[i].id}"></a>${sections[i].dataset.name}</li>`;
+    const list = `<li><a class="links_menu" href="#${sections[i].id}">${sections[i].dataset.name}</a></li>`;
     manyList += list;      
 }
 //list is add it to the nav
@@ -16,17 +17,37 @@ nav.innerHTML = manyList;
 //scroll to section
 const linkes = document.querySelectorAll('a')
 linkes.forEach(link => {
-    link.addEventListener('click', toSection())
+    link.addEventListener('click', toSection)
 })
 
-function toSection() {
+function toSection(e) {
+	e.preventDefault();// To prevent the default action when an anchor tag is clicked
     sections.forEach(section => {
-        const sectionsIds = section.id
+    	if(e.target.href.search(section.id) != -1) { //check if the href contains the section id, if yes, then only scroll
+        	section.scrollIntoView({behavior: "smooth"});
+    	}
     })
-    sectionsIds.scrollIntoView();
 }
 
 //3
+    //activClasses = document.querySelectorAll('.active')
+   sections.forEach(sect => {
+        sect.addEventListener('scroll', activ)
+    })
+
+    function activ() {
+        sections.forEach(clas => {
+            const view = clas.getBoundingClientRect()
+            if (view.top < 50 && view.top >= -650){
+                clas.classList.add("active");
+            }
+            else{
+                clas.classList.remove("active"); 
+        }
+    })
+};
+
+//4
 //function to go back to the top
 function goToTop() {
     document.documentElement.scrollTop = 0;
