c8fc97320a569a2a6cebab8c443803ed156698d6
humberto1212 <71103179+humberto1212@users.noreply.github.com>
2020-12-15 12:03:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
-//Global Variables
-const sections = document.querySelectorAll('section')
+//Global variables
+const sections = document.querySelectorAll('section');
 const nav = document.getElementById('nav_list');
-let manyList = ''
-           
+const links = document.querySelectorAll('a');
+let navList = '';
 
 // 1
-//list creation with class, href and dataset
+//list creation with class, href and dataset           
 for (var i = 0; i < sections.length; i++) {
     const list = `<li><a class="links_menu" href="#${sections[i].id}">${sections[i].dataset.name}</a></li>`;
-    manyList += list;      
+    navList += list;      
 }
-//list is add it to the nav
-nav.innerHTML = manyList;
+
+nav.innerHTML = navList;//list is add it to the nav
 
 // 2
 //scroll to section
-const linkes = document.querySelectorAll('a')
-linkes.forEach(link => {
-    link.addEventListener('click', toSection)
+links.forEach(link => {
+    link.addEventListener('click', toSection);
 })
 
 function toSection(e) {
-	e.preventDefault();// To prevent the default action when an anchor tag is clicked
+	e.preventDefault();// To prevent the default action 
     sections.forEach(section => {
     	if(e.target.href.search(section.id) != -1) { //check if the href contains the section id, if yes, then only scroll
         	section.scrollIntoView({behavior: "smooth"});
@@ -30,22 +29,20 @@ function toSection(e) {
 }
 
 //3
-    //activClasses = document.querySelectorAll('.active')
-   sections.forEach(sect => {
-        sect.addEventListener('scroll', activ)
-    })
-
-    function activ() {
-        sections.forEach(clas => {
-            const view = clas.getBoundingClientRect()
-            if (view.top < 50 && view.top >= -650){
-                clas.classList.add("active");
-            }
-            else{
-                clas.classList.remove("active"); 
+//Add and remove activ Classes 
+function activ() {
+    sections.forEach(clas => {
+        const view = clas.getBoundingClientRect()
+        if (view.top < 50 && view.top >= -800){
+            clas.classList.add("active");
+        }
+        else{
+            clas.classList.remove("active"); 
         }
     })
 };
+
+window.addEventListener('scroll', activ);
 
 //4
 //function to go back to the top
